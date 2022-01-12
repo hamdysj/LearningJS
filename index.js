d0d76d4b8d78314e4305660fb9eb91d1e414083f
Hamdallah Sulaimon <hamdysj@gmail.com>
2022-01-12 12:44:50 +0100

    update

--- index.js
+++ index.js
@@ -14,3 +14,27 @@ function holidays(arr) {
     }
  return "Have a great day!";
 }
+
+//Uncomment these to check your work!
+const months = ["April", "May", "June", "October"];
+const animals = ["Cats", "Dogs", "Pigs"];
+console.log(holidays(months)); // should return: "Happy Halloween"
+console.log(holidays(animals)); // should return: "Have a great day!"
+
+/*
+ * 
+ * Using a FOR loop, write a function addN which adds the argument n to each number in the array arr and returns the updated arr.
+ */
+
+function addN(arr, n){
+	  // ADD CODE HERE
+	  for(let i = 0; i<arr.length; i++)
+	    {
+	      arr[i] = arr[i] + n;
+	    }
+	  return arr;
+	} 
+
+	// Uncomment these to check your work!
+	console.log(addN([1, 2, 3], 3)); // expected log [4, 5, 6]
+	console.log(addN([3, 4, 5], 2)); // expected log [5, 6, 7]
