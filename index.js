5d11b6925102141eec4024a587aa12f323bbf43d
Hamdallah Sulaimon <hamdysj@gmail.com>
2022-01-21 10:56:34 +0100

    update

--- index.js
+++ index.js
@@ -55,3 +55,80 @@ function addN(arr, n){
 		// Uncomment these to check your work!
 		console.log(getTheSum([3, 6, 9])); // expected log 18
 		console.log(getTheSum([10, 11, 12])); // expected log 33
+		
+		
+/*
+ * Create a function multiplyAll that takes an unknown number of integer arguments,
+ *  multiplies them all together, and returns the result.
+ *  */	
+		
+		// ADD CODE HERE 
+		function multiplyAll(...value)
+		{
+		  let total = 1;
+		  
+		  for(let i = 0; i<value.length; i++)
+		    {
+		     total = total * value[i];
+		    }
+		  
+		  return total;
+		}
+
+		// Uncomment these to check your work!
+		console.log(multiplyAll(9, 4, 5, 6, 7, 2, 1, 8, 3)) // should log: 362880
+		console.log(multiplyAll(5, 5, 5, 3)) // should log: 375
+
+		
+/*Write a function mergingElements which adds each element in array1 
+ * to the corresponding element of array2 and returns the new array.
+ * */
+
+  //create function
+const mergingElements = (arr1, arr2) => {
+  //create a new array
+  const arr3 = [];
+  //iterate through the arrays
+  for(let i = 0; i<arr1.length; i++)
+    {
+      //Add element in Array 1 to it correspondence
+      arr3.push(arr1[i] + arr2[i]);
+    }
+  
+    //return the new array
+  return arr3;
+}
+// Uncomment these to check your work!
+console.log(mergingElements([1, 2, 3, 4], [5, 6, 7, 8])); // expected log [6, 8, 10, 12]
+console.log(mergingElements([7, 3, 6, 0], [3, 9, 17, 81])); // expected log [10, 12, 23, 81]
+
+
+
+/*
+ * 
+ *Write a function mergingTripletsAndQuints which takes in two arrays as arguments.
+ *  This function will return a new array replacing the elements in array1 if they are divisible by 3 or 5.
+ *  The number should be replaced with the sum of itself added to the element at the corresponding index in array2.
+ */
+
+
+function mergingTripletsAndQuints(array1, array2) {
+	 // ADD CODE HERE
+	  //for loop iteration
+	  for(let i = 0; i<array1.length; i++)
+	    {
+	       //return a new array if element is divisible by 3 or 5
+	      let value = array1[i] % 3;
+	      let value1 = array1[i] % 5;
+	      if(value === 0 || value1 === 0 )
+	        {
+	          array1[i] = array1[i] + array2[i];
+	        }
+	    }
+	  
+	  return array1;
+	}
+
+	// Uncomment these to check your work!
+	console.log(mergingTripletsAndQuints([1, 2, 3, 4, 5, 15], [1, 3, 6, 7, 8, 9])); // expected log [1, 2, 9, 4, 13, 24]
+	console.log(mergingTripletsAndQuints([1, 1, 3, 9, 5, 15], [1, 2, 3, 4, 5, 6])); // expected log [1, 1, 6, 13, 10, 21]
